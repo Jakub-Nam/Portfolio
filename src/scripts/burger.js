97d20a46791764923402dfbb27cb2da27b01adae
John Doe <kubanam1995@gmail.com>
2020-08-31 20:54:13 +0200

    The burger button was added

---
+++ src/scripts/burger.js
@@ -0,0 +1,21 @@
+
+const burger = document.querySelector('#nav-icon1');
+const navUl = document.querySelector('.nav__ul')
+const iconsNav = document.querySelector('.nav__icons-wrapper')
+burger.addEventListener('click', () => {
+    burger.classList.toggle("open")
+
+    const ulStyle = window.getComputedStyle(navUl, null).getPropertyValue("left");
+    if (ulStyle !== '0px') {
+        navUl.style.left ='0%'
+        iconsNav.style.bottom ='0%'
+    } else {
+        navUl.style.left ='-100%'
+        iconsNav.style.bottom ='-10%'
+    }
+
+
+    }   
+)
+
+
